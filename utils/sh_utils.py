19e3698e5fd68d81ec6835ecb5281108c24fc87b
Cezychan <127017871+Cezychan@users.noreply.github.com>
2025-12-31 01:33:53 +0800

    Update sh_utils.py

--- utils/sh_utils.py
+++ utils/sh_utils.py
@@ -112,7 +112,7 @@ def eval_sh(deg, sh, dirs):
     return result
 
 def RGB2SH(rgb):
-    return (rgb - 0.5) / C0
+    return (rgb - 0.5) / C0            #先减0.5，规定区间变为（-0.5，0.5）；再乘直流分量系数，达到规定范围
 
 def SH2RGB(sh):
     return sh * C0 + 0.5
